b5f8edc5324147146e21fd246da7d0286bf2ff66
Guard-SK <81436587+Guard-SK@users.noreply.github.com>
2021-06-16 16:19:05 +0200

    update

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ prefix = "a3"
 bot = commands.Bot(command_prefix = prefix, intents=intents)
 AlistIDs = [431116940568952842, 544573811899629568]
 
+
 OWNER_ID = 544573811899629568
 
 @bot.command()
@@ -35,9 +36,12 @@ for filename in os.listdir("./cogs"):
         bot.load_extension(f"cogs.{filename[:-3]}")
         print("Cog loaded")
 
+with open("token.0", "r", encoding="utf-8") as f:
+    TOKEN = f.read()
+
 @bot.event
 async def on_ready():
     print("R3NAUT Announcements!")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="https://github.com/Guard-SK/R3NAUT-Announcements"))
 
-bot.run("ODUyMjM5MjQyNjgzNDgyMTEy.YMD7pQ.jgZahaBnAQj5L9QcL2pRdVcqWhs") #taking token from Heroku os.environ['DISCORD_TOKEN']
+bot.run(TOKEN) #taking token from Heroku os.environ['DISCORD_TOKEN']
